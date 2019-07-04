d792e2418380215407c5b33b4fde15a9efa6eaab
Scott Godwin <sgodwincs@gmail.com>
2019-07-03 23:19:28 -0700

    Export UTF-8 charset

--- src/charsets/mod.rs
+++ src/charsets/mod.rs
@@ -5,3 +5,7 @@ pub use us_ascii::{
     Alias as UsAsciiAlias, Character as UsAsciiCharacter, Charset as UsAsciiCharset,
     DecodeError as UsAsciiDecodeError, Str as UsAsciiStr, String as UsAsciiString,
 };
+pub use utf_8::{
+    Alias as Utf8Alias, Character as Utf8Character, Charset as Utf8Charset,
+    DecodeError as Utf8DecodeError, Str as Utf8Str, String as Utf8String,
+};
